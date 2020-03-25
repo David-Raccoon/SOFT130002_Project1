9b9ea420d1c8cdd2a7856549ce7368b30bc02b89
Wang <18307130214@fudan.edu.cn>
2020-03-26 00:11:34 +0800

    1st commit

---
+++ src/js/cities.js
@@ -0,0 +1,7 @@
+var cities = {
+    "Country: All": ["City: All"],
+    "China": ["City: All", "Shanghai", "Kunming", "Beijing", "Yantai"],
+    "Japan": ["City: All", "Tokyo", "Osaka", "Kamakura"],
+    "Italy": ["City: All", "Roma", "Milan", "Venice", "Florence"],
+    "America": ["City: All", "New York", "San Francisco", "Washington"]
+}
